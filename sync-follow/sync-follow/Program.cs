b921ba6e105c1e3e1ef55a8cef288d291a7a188d
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-01-30 01:30:47 +0900

    my machine is crash. so rescue

--- sync-follow/sync-follow/Program.cs
+++ sync-follow/sync-follow/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using CoreTweet;
+using CoreTweet.Streaming;
 
 namespace sync_follow
 {
     class Program
     {
+        private static XmlSerializer x_ = null;
+        private static XmlSerializer x
+        {
+            get
+            {
+                if (x_ == null)
+                {
+                    x_ = new XmlSerializer(typeof(Tokens));
+                }
+                return x_;
+            }
+        }
+        private static Dictionary<string, Tokens> tokens_ = null;
+        private static Tokens users(string sn)
+        {
+            if (!tokens_.ContainsKey(sn))
+            {
+                var file = sn + ".xml";
+                if (File.Exists(file))
+                {
+                    using (var y = File.OpenRead(file))
+                    {
+                        tokens_.Add(sn, x.Deserialize(y) as Tokens);
+                    }
+                }
+                else
+                {
+                    Console.Write("CK> ");
+                    var CK = Console.ReadLine();
+                    Console.Write("CS> ");
+                    var CS = Console.ReadLine();
+                    var se = OAuth.Authorize(CK, CS);
+                    Console.WriteLine(se.AuthorizeUri);
+                    Console.Write("pin> ");
+                    tokens_.Add(sn, se.GetTokens(Console.ReadLine()));
+                    using (var y = File.Open(file, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        x.Serialize(y, tokens_[sn]);
+                    }
+                }
+            }
+            return tokens_[sn];
+        }
         static void Main(string[] args)
         {
+            var PG_nonen = users("PG_nonen");
+            var masakazu_sub0 = users("masakazu_sub0");
+            var main_follow = PG_nonen.Friends.List().ToList();
+            var sub_follow = masakazu_sub0.Friends.List().ToList();
+            var list = PG_nonen.Lists.Show(slug => "masakazu-following", owner_screen_name => "PG_nonen");
+            foreach (var follow in main_follow)
+            {
+                if ( !sub_follow.Contains(follow))
+                {
+                    masakazu_sub0.Friendships.Update();
+                }
+            }
         }
     }
 }
---
+++ tweet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using CoreTweet;
+using CoreTweet.Streaming;
+
+namespace tweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new XmlSerializer(typeof(Tokens));
+            var file = args [ 0 ] + ".xml";
+            Tokens tokens;
+            if (File.Exists(file))
+            {
+                using (var y = File.OpenRead(file))
+                {
+                    tokens = x.Deserialize(y) as Tokens;
+                }
+            }
+            else
+            {
+                Console.Write("CK> ");
+                var CK = Console.ReadLine();
+                Console.Write("CS> ");
+                var CS = Console.ReadLine();
+                var se = OAuth.Authorize(CK, CS);
+                Console.WriteLine(se.AuthorizeUri);
+                Console.Write("pin> ");
+                tokens = se.GetTokens(Console.ReadLine());
+                using (var y = File.Open(file, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    x.Serialize(y, tokens);
+                }
+            }
+            args
+            tokens.Statuses.Update(status => args[1] + args[2]);
+        }
+    }
+}
